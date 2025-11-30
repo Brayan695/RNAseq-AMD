c56f6aee439dceb4089a585807f991a6861c2e96
Brayan Gutierrez <68131493+Brayan695@users.noreply.github.com>
2025-11-30 13:44:52 -0600

    Update Robustness Check.R

--- Scripts/Robustness Check.R
+++ Scripts/Robustness Check.R
@@ -13,21 +13,6 @@ big_data = read.csv("C:/Users/Brayan Gutierrez/Desktop/RNAseq-AMD/Dataset/gene_i
 cont_late = subset(big_data, mgs_level %in% c("MGS1", "MGS4"))
 
 
-library(RColorBrewer)
-library(MEGENA)
-library(visNetwork)
-library(readr)
-library(igraph)
-library(dplyr)
-
-
-# Load and filter data
-
-big_data = read.csv("C:/Users/Brayan Gutierrez/Desktop/RNAseq-AMD/Dataset/gene_input.csv",
-                    row.names = 1, check.names = FALSE)
-cont_late = subset(big_data, mgs_level %in% c("MGS1", "MGS4"))
-
-
 # Divergence + MEGENA function
 
 JSD_megena_run <- function(expr_mat) {
